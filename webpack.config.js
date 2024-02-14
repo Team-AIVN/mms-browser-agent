843bbebf2711a3d74cb2787f814e1db3cb9e9474
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2024-02-14 16:03:07 +0100

    Add jQuery Don't even know if this is necessary...

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -21,6 +22,10 @@ const config = {
         new HtmlWebpackPlugin({
             template: "index.html",
         }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+        })
 
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
